7dfcda469d3ebca5e4952b89537c8a7096a92d9f
Huy Le <lehuydit@gmail.com>
2022-04-22 07:54:40 +0700

    sample 2 librarys and demo

---
+++ mylibrary/src/main/java/com/huyle/mylibrary/Counter.java
@@ -0,0 +1,15 @@
+package com.huyle.mylibrary;
+/**
+ *
+ */
+public class Counter {
+    private int value;
+
+    public int increment() {
+        return ++value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ mylibrary2/src/main/java/com/huyle/mylibrary2/Counter2.java
@@ -0,0 +1,13 @@
+package com.huyle.mylibrary2;
+
+public class Counter2 {
+    private int value;
+
+    public int increment() {
+        return ++value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
