33212b02a074c4bec611edf2cc5fdabefda17a32
Huy Le <lehuydit@gmail.com>
2022-04-23 08:22:47 +0700

    more libs

---
+++ android-toast-lib/src/main/java/com/test/identify/toast/Counter10.java
@@ -0,0 +1,15 @@
+package com.test.identify.toast;
+
+public class Counter10 {
+
+    private int value;
+
+    public int increment() {
+        return ++value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
